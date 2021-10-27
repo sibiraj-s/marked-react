3d6397e2d2b035dbb24546b81ab71fceb6f02bc5
sibiraj-s <dev.sibiraj@outlook.com>
2021-10-27 09:31:18 +0530

    Remove invalid exports

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,3 @@
 import Markdown from './Markdown';
-import ReactParser from './ReactParser';
-
-export const { parser } = ReactParser;
-export const { parseInline } = ReactParser;
 
 export default Markdown;
