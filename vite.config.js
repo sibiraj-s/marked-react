1befe2c4c6ac67b40d5f29c2aa48aeb39bc87f0e
sibiraj-s <dev.sibiraj@outlook.com>
2021-10-30 05:56:58 +0530

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'demo',
   server: {
     port: 5678,
   },
   build: {
-    outDir: './demo/build',
+    outDir: 'build',
   },
   plugins: [
     react(),
