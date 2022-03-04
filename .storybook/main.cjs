9ba72d2f1de8baa948a6f47eb503b5be227af7f7
Sibiraj <dev.sibiraj@outlook.com>
2022-03-04 21:25:43 +0530

    Remove unwanted storybook addons

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,9 +4,7 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-controls',
   ],
   framework: '@storybook/react',
 };
