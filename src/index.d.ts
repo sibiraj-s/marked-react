53a2ed2335789473080f3b8eee9fca65e2e7aef8
Sibiraj <dev.sibiraj@outlook.com>
2022-05-07 13:12:31 +0530

    Fix renderer types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,5 @@
 declare module 'marked-react' {
-  import React, { ReactElement } from 'react';
-
-  type RendererFunction = (...args: any[]) => ReactElement | null;
+  import { ReactElement, FC, ReactNode } from 'react';
 
   export interface MarkdownOptions {
     breaks?: boolean;
@@ -9,14 +7,45 @@ declare module 'marked-react' {
     baseURL?: string;
     openLinksInNewTab?: boolean;
     langPrefix?: string;
-    renderer?: Record<string, RendererFunction>;
+    renderer?: ReactRenderer | RendererObject;
+  }
+
+  type RendererObject = Partial<Omit<ReactRenderer, 'elementId'>>;
+
+  class ReactRenderer {
+    elementId: number;
+
+    constructor(options?: MarkdownOptions);
+    heading(children: any, level: any): ReactElement;
+    paragraph(children: any): ReactElement;
+    link(href: any, text: any): ReactElement;
+    image(href: any, text: any, title: any): ReactElement;
+    codespan(code: any, lang: any): ReactElement;
+    code(code: any, lang: any): ReactElement;
+    blockquote(children: any): ReactElement;
+    list(children: any, ordered: any): ReactElement;
+    listItem(children: any): ReactElement;
+    checkbox(checked: any): ReactElement;
+    table(children: any): ReactElement;
+    tableHeader(children: any): ReactElement;
+    tableBody(children: any): ReactElement;
+    tableRow(children: any): ReactElement;
+    tableCell(children: any, flags: any): ReactElement;
+    strong(children: any): ReactElement;
+    em(children: any): ReactElement;
+    del(children: any): ReactElement;
+    text(text: any): any;
+    html(html: any): any;
+    hr(): ReactElement;
+    br(): ReactElement;
   }
 
   interface Props extends MarkdownOptions {
     value?: string;
+    children?: ReactNode;
   }
 
-  const Markdown: React.FC<Props>;
+  const Markdown: FC<Props>;
 
   export default Markdown;
 }
