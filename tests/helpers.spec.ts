f0ac0ad46784a86d2059f31e1bb79624e796332c
Wang Yi <1721436982@qq.com>
2023-06-02 23:53:40 +0800

    Complete mergeProps unit testing

--- tests/helpers.spec.ts
+++ tests/helpers.spec.ts
@@ -1,6 +1,6 @@
 import { it, expect, describe } from '@jest/globals';
 
-import { joinBase, unescape } from '../src/helpers';
+import { joinBase, unescape, mergeProps } from '../src/helpers';
 
 describe('Helpers', () => {
   it('should joinBase correctly', () => {
@@ -17,5 +17,12 @@ describe('Helpers', () => {
     expect(unescape('&#39;')).toBe("'");
     expect(unescape('')).toBe('');
     expect(unescape()).toBe('');
+  });
+
+  it('should mergeProps correctly', () => {
+    expect(mergeProps({ value: 1 }, { value: 2 })).toEqual({ value: 2 });
+    expect(mergeProps({ key: 'number' }, { value: 2 })).toEqual({ value: 2, key: 'number' });
+    expect(mergeProps({ value: 1 }, { value: undefined })).toEqual({ value: 1 });
+    expect(mergeProps({ value: 1, key: 'number' }, { value: undefined })).toEqual({ value: 1, key: 'number' });
   });
 });
